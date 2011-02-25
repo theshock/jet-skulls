50c5eddc88bdbf9abb6b5f2811197e796b462cc6
Shock <shocksilien@gmail.com>
2011-02-25 17:31:21 +0200

    Floating unit speed linked to health

--- server/classes/Unit.js
+++ server/classes/Unit.js
@@ -3,7 +3,7 @@ new function () {
 var SQRT_2 = (2).sqrt();
 
 GLOBAL.Unit = atom.Class({
-	speed: 150,
+	_speed: 150,
 	lastUpdate: 0,
 	lastShot  : 0,
 	radius    : 7,
@@ -15,9 +15,23 @@ GLOBAL.Unit = atom.Class({
 		this.field      = field;
 		this.position   = new Point(field.randomPoint());
 	},
+	get speed () {
+		return this._speed * (this.health > 25 ? 1 : 0.5);
+	},
 	update: function (status) {
 		var now  = Date.now();
 		var time = this.updateTime();
+		
+		// mouse
+		if (status.mouse) {
+			this.angle = Point.from(status.mouse).angleTo(this.position);
+
+			// shot
+			if (status.shot && this.lastShot + this.weaponReload < now) {
+				this.lastShot = now;
+				this.field.shoot(status.mouse, this);
+			}
+		}
 
 		// move
 		var x = (status.move.x.limit(-1, 1) * this.speed * time);
@@ -25,7 +39,7 @@ GLOBAL.Unit = atom.Class({
 
 		if (x || y) {
 			if (x && y) {
-				// todo: Использовать нормальную формулу
+				// У нас движение НЕ завязано на мышь
 				x /= SQRT_2;
 				y /= SQRT_2;
 				var distance = Math.hypotenuse(x, y);
@@ -41,18 +55,6 @@ GLOBAL.Unit = atom.Class({
 					this.position.move(dir[i]);
 					break;
 				}
-			}
-		}
-		
-
-		// mouse
-		if (status.mouse) {
-			this.angle = Point.from(status.mouse).angleTo(this.position);
-
-			// shot
-			if (status.shot && this.lastShot + this.weaponReload < now) {
-				this.lastShot = now;
-				this.field.shoot(status.mouse, this);
 			}
 		}
 	},
